2fd1a282eacc23e572019c4bc4d664eb635e7d86
ShubhamShanker510 <78153738+ShubhamShanker510@users.noreply.github.com>
2025-05-31 14:35:22 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,11 +13,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  }));
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://snapshopecommerce.netlify.app'
+  ];
   
+  app.use(cors({
+    origin: function(origin, callback) {
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }));
 
 
 app.use('/api/user',userRoute)
